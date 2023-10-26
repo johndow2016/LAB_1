94b2fcf5bb22972fb016b1223636608b563b1b7b
johndow2016 <148933428+johndow2016@users.noreply.github.com>
2023-10-26 22:32:05 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -16,5 +16,43 @@ def data_to():
   some_str = 'Hello my dear friends!'
 some_value = 10
 #передаем данные в шаблон и вызываем его
-  return render_template('simple.html',some_str = some_str,
+return render_template('simple.html',some_str = some_str,
 some_value = some_value,some_pars=some_pars)
+# модули работы с формами и полями в формах
+from flask_wtf import FlaskForm,RecaptchaField
+from wtforms import StringField, SubmitField, TextAreaField
+# модули валидации полей формы
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+# используем csrf токен, можете генерировать его сами
+SECRET_KEY = 'secret'
+app.config['SECRET_KEY'] = SECRET_KEY
+# используем капчу и полученные секретные ключи с сайта Google
+app.config['RECAPTCHA_USE_SSL'] = False
+app.config['RECAPTCHA_PUBLIC_KEY'] = 'сюда поместить ключ из google'
+app.config['RECAPTCHA_PRIVATE_KEY'] = 'сюда поместить секретный ключ из google'
+app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
+# обязательно добавить для работы со стандартными шаблонами
+from flask_bootstrap import Bootstrap
+bootstrap = Bootstrap(app)
+# создаем форму для загрузки файла
+class NetForm(FlaskForm):
+# поле для введения строки, валидируется наличием данных
+# валидатор проверяет введение данных после нажатия кнопки submit
+# и указывает пользователю ввести данные, если они не введены
+# или неверны
+openid = StringField('openid', validators = [DataRequired()])
+# поле загрузки файла
+# здесь валидатор укажет ввести правильные файлы
+upload = FileField('Load image', validators=[
+FileRequired(),
+FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+# поле формы с capture
+recaptcha = RecaptchaField()
+#кнопка submit, для пользователя отображена как send
+submit = SubmitField('send')
+# функция обработки запросов на адрес 127.0.0.1:5000/net
+# модуль проверки и преобразование имени файла
+# для устранения в имени символов типа / и т.д.
+from werkzeug.utils import secure_filename
+import os
