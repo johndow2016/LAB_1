d4e1ce4ff50970eb91652d5ca9f06739b5d2548c
johndow2016 <148933428+johndow2016@users.noreply.github.com>
2023-10-26 22:18:14 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -16,5 +16,5 @@ def data_to():
   some_str = 'Hello my dear friends!'
 some_value = 10
 #передаем данные в шаблон и вызываем его
-return render_template('simple.html',some_str = some_str,
-some_value = some_value,some_pars=some_pars)
+  return render_template('simple.html',some_str = some_str,
+  some_value = some_value,some_pars=some_pars)
