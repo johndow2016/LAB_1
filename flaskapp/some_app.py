f7018c9c7917b0424a3d38014afe483d1538dff0
johndow2016 <148933428+johndow2016@users.noreply.github.com>
2023-10-27 00:26:09 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -82,7 +82,7 @@ for elem in decode:
 form.upload.data.save(filename)
 # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
 # сети, если был нажат сабмит, либо передадим falsy значения
-return render_template('net.html',form=form,image_name=filename,neurodic=neurodic)
+return render_template('net.html', form = form, image_name = filename, neurodic = neurodic)
 from flask import request
 from flask import Response
 import base64
