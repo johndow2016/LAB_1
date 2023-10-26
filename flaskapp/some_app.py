bcea26aba59eb57963c58c95aec597f6ea7b3dca
johndow2016 <148933428+johndow2016@users.noreply.github.com>
2023-10-26 22:40:24 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -41,7 +41,7 @@ class NetForm(FlaskForm):
 # валидатор проверяет введение данных после нажатия кнопки submit
 # и указывает пользователю ввести данные, если они не введены
 # или неверны
-openid = StringField('openid', validators = [DataRequired()])
+  openid = StringField('openid', validators = [DataRequired()])
 # поле загрузки файла
 # здесь валидатор укажет ввести правильные файлы
 upload = FileField('Load image', validators=[
