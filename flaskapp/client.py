a4505c6f115736ca6928e26c057cd9064825a792
johndow2016 <148933428+johndow2016@users.noreply.github.com>
2023-10-27 22:37:06 +0700

    Update client.py

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -1,3 +1,10 @@
+import requests
+r = requests.get('http://localhost:5000/')
+print(r.status_code)
+print(r.text)
+r = requests.get('http://localhost:5000/data_to')
+print(r.status_code)
+print(r.text)
 # импортируем нужные модули
 import os
 from io import BytesIO
